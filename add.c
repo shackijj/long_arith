9130796de96c499df1f299fcff2d1b91ae8ce489
shackijj <shaackiy@gmail.com>
2016-04-06 20:49:50 +0300

    addiction works

--- add.c
+++ add.c
@@ -20,25 +20,35 @@ int *init_long(int from) {
     return to;
 }
 
+void print_long( int *l ) {
+    printf("Length is: %d\n", l[0]);
+    int i;
+    for(i = l[0]; i > 0; i--) {
+        printf("%d", l[i]);
+    }
+    printf("\n");
+}
+
 
 int *add(int *a, int *b) {
-    int i, 
-        rem = 0;
+    int i, add_res, rem = 0;
     int *res = calloc(SIZE, sizeof(int));
-    memcpy(&res, &a, sizeof(a[0]));
-    
-    for(i = 1; i < b[0]; i++) {
-        if (i < res[0]) {
-            int add_res = res[i] + b[i] + rem;
+    memcpy(res, a, (a[0] + 1) * sizeof(int));
+   
+    for(i = 1; i <= b[0]; i++) {
+        if (i <= res[0]) {
+            add_res = res[i] + b[i] + rem;
             if (add_res > 9) {
                 rem = add_res / 10;
             } else {
                 rem = 0;
             }
             res[i] = add_res % 10;
+            // pintf("Added %d, rem %d\n", add_res % 10,  rem);
+
         } else {
             if (rem) {
-                int add_res = b[i] + rem;
+                add_res = b[i] + rem;
                 if (add_res > 9) {
                     rem = add_res / 10;
                 } else {
@@ -54,12 +64,59 @@ int *add(int *a, int *b) {
 
     if (rem) {
         res[0]++;
-        res[res[0] - 1] = rem;
+        res[res[0]] = rem;
     }
 
     return res;
 }
 
+int *multiply(int *a, int *b) {
+    int *res = init_long(0);
+    int i = 0,
+        tmp_res = 0,
+        rem = 0,
+        j = 0,
+        tmp_add;
+
+    for(i = 1; i <= a[0]; i++) {
+        for( j = 1; j <= b[0]; j++ ) {
+            tmp_res = (a[i] * b[j]) + rem;
+
+            if (tmp_res > 9) {
+                rem = tmp_res / 10;
+            } else {
+                rem = 0;
+            }
+            printf("tmp_res %d, rem %d\n", tmp_res, rem);
+ 
+            if (res[i + j - 1]) {
+                tmp_add = res[i + j - 1] + tmp_res + rem;
+                if (tmp_add > 9) {
+                    rem = tmp_add / 10;
+                }
+                printf("Writing %d rem %d\n", tmp_add % 10, rem);
+                res[i + j] = tmp_add % 10;
+                
+            } else {
+                printf("Writing %d rem %d\n", tmp_res % 10,  rem);
+                res[i + j - 1] = tmp_res % 10;
+                rem = tmp_res / 10;
+                res[0]++;
+            }
+        }
+        //print_long(res);
+        if (rem) {
+            printf("Writing reminer %d\n", rem);
+            res[0]++;
+            res[res[0]] = rem;
+        }
+        printf("End of first %d digit\n", i);
+        print_long(res);
+    }
+    print_long(res);
+    return res;
+}
+            
 int main() {
 
     int *al;
@@ -73,16 +130,11 @@ int main() {
 
     al = init_long(a);
     bl = init_long(b);
-    res = add(al, bl);
-    
-    for(i = 0; i < res[0]; i++) {
-        printf("%d", res[i]);
-    }
 
-    printf("\n");
-
-    free(al);
-    free(bl);
-    free(res);
+    res = multiply(al, bl);
+   
+    if(al) free(al);
+    if(bl) free(bl);
+    if(res) free(res);
     return 0;
 }
